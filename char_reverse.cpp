82bbb2935d361e31ec4ac4099e771f09907c55fa
Amulya0929 <85796005+Amulya0929@users.noreply.github.com>
2021-07-18 20:24:51 +0530

    Add files via upload

---
+++ char_reverse.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2,ch3;
+	printf("Enter a three characters:");
+	scanf("%c%c%c",&ch1,&ch2,&ch3);
+	printf("the reverse of %c%c%c is %c%c%c");
+	return 0;
+	
+}
