4dd6a662a3d056a257c8ef032d475300d3b5ce42
Amulya0929 <85796005+Amulya0929@users.noreply.github.com>
2021-07-18 20:25:33 +0530

    Add files via upload

---
+++ complex.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct complex
+{
+	int real;
+	int imaginary;
+}c;
+int main()
+{
+	struct complex c1;
+	printf("\n Enter real part :");
+	scanf("%d",&c.real);
+	printf("\n Enter real part :");
+	scanf("%d",&c1.real);
+	printf("\n Enter Imaginary part :");
+	scanf("%d",&c.imaginary);
+	printf("\n Enter imaginary part :");
+	scanf("%d",&c1.imaginary);
+	printf("\n The complex number is : %d + i %d",c.real,c.imaginary);
+	printf("\n The complex number is : %d + i %d",c1.real,c1.imaginary);
+}
