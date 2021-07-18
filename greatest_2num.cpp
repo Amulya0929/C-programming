60580157771d39d6c1b08fa1ff49143a6b9e34c6
Amulya0929 <85796005+Amulya0929@users.noreply.github.com>
2021-07-18 20:27:51 +0530

    Add files via upload

---
+++ greatest_2num.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two numbers:");
+	scanf("%d%d",&a,&b);
+	if (a>b)
+	printf("\n a=%d is greater ");
+	else 
+	printf("\n b=%d is greater ");
+	return 1;
+	
+}
