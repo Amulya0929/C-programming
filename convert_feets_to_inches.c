0660288c5404fa74148c1bcf913869da285c13ff
Amulya0929 <85796005+Amulya0929@users.noreply.github.com>
2021-07-20 18:59:14 +0530

    Add files via upload

---
+++ convert_feets_to_inches.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int feet,inches;
+	printf("Enter the value in feets:");
+	scanf("%d",&feet);
+	inches = feet *12;
+	printf("Total inches will be %d",inches);
+	return 0;
+}
